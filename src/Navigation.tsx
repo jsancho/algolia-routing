8e8c4f8184e51ecc838316093ae0fe40f77be1fa
Jaume Sancho <jaume.sancho@vehnet.com>
2023-01-21 14:24:51 +0100

    initialise algolia client and router as a singleton

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,9 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NavMenu } from "components/layout/NavMenu";
 import { AlgoliaContainer } from "./AlgoliaContainer";
+import { history } from "instantsearch.js/es/lib/routers";
+import simple from "lib/simple";
+import algoliasearch from "algoliasearch";
 import "./navigation.css";
 
 const indexes = ["stays", "workjobs"];
+
+const routing = {
+  router: history(),
+  stateMapping: simple()
+};
+
+const client = algoliasearch("14LH54VIYV", "20ec51f3a8dd72e5d6a6e0e79278dbde");
 
 function Navigation() {
   return (
@@ -16,7 +26,7 @@ function Navigation() {
             <Route
               key={`route-${index}`}
               path={`/${index}`}
-              element={<AlgoliaContainer index={index} />}
+              element={<AlgoliaContainer index={index} client={client} routing={routing} />}
             />
           );
         })}
