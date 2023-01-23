9de24ad45df003370f1631452370db93b4a37dc8
Jaume Sancho <jaume.sancho@vehnet.com>
2023-01-23 09:23:19 +0100

    removed algolia dependencies from Navigator component

--- src/AlgoliaContainer.tsx
+++ src/AlgoliaContainer.tsx
@@ -1,22 +1,27 @@
+import algoliasearch from "algoliasearch";
 import { InstantSearch } from "react-instantsearch-hooks-web";
+import { history } from "instantsearch.js/es/lib/routers";
+import simple from "lib/simple";
 import { Layout } from "components/layout/Layout";
 import { VirtualSortBy } from "components/virtual/VirtualSortBy";
 import { getSortItems } from "data/getSortItems";
-import { RouterProps } from "instantsearch.js/es/middlewares";
-import { UiState } from "instantsearch.js";
-import { SearchClient } from "algoliasearch";
 
 interface IProps {
   index: string;
-  client: SearchClient;
-  routing: RouterProps<UiState, UiState>;
 }
 
-export const AlgoliaContainer = ({ index, client, routing }: IProps) => {
+const searchClient = algoliasearch("14LH54VIYV", "20ec51f3a8dd72e5d6a6e0e79278dbde");
+
+const routing = {
+  router: history(),
+  stateMapping: simple()
+};
+
+export const AlgoliaContainer = ({ index }: IProps) => {
   const sortOptions = getSortItems(index);
 
   return (
-    <InstantSearch indexName={index} searchClient={client} routing={routing}>
+    <InstantSearch indexName={index} searchClient={searchClient} routing={routing}>
       {/* Add virtualised date filter dropdowns */}
       {/* Add virtualised refinement list */}
       <VirtualSortBy sortOptions={sortOptions} />
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,19 +1,9 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NavMenu } from "components/layout/NavMenu";
 import { AlgoliaContainer } from "./AlgoliaContainer";
-import { history } from "instantsearch.js/es/lib/routers";
-import simple from "lib/simple";
-import algoliasearch from "algoliasearch";
 import "./navigation.css";
 
 const indexes = ["stays", "workjobs"];
-
-const routing = {
-  router: history(),
-  stateMapping: simple()
-};
-
-const client = algoliasearch("14LH54VIYV", "20ec51f3a8dd72e5d6a6e0e79278dbde");
 
 function Navigation() {
   return (
@@ -26,7 +16,7 @@ function Navigation() {
             <Route
               key={`route-${index}`}
               path={`/${index}`}
-              element={<AlgoliaContainer index={index} client={client} routing={routing} />}
+              element={<AlgoliaContainer index={index} />}
             />
           );
         })}
